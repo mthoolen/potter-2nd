57bba999ac598e81ca337f72d41de51814cc15d0
Roy Straub <roy.straub@codesquad.nl>
2020-12-13 20:28:55 +0100

    Added canary tests

--- src/test/java/nl/codesquad/library/java/HelloTest.java
+++ src/test/java/nl/codesquad/library/java/HelloTest.java
@@ -1,4 +1,12 @@
 package nl.codesquad.library.java;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
 public class HelloTest {
+    @Test
+    public void canary() {
+        assertTrue(false);
+    }
 }
--- src/test/kotlin/nl/codesquad/library/kotlin/HelloTest.kt
+++ src/test/kotlin/nl/codesquad/library/kotlin/HelloTest.kt
@@ -2,7 +2,11 @@ package nl.codesquad.library.kotlin
 
 import org.junit.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class HelloTest {
-
+    @Test
+    fun canary() {
+        assertTrue(false)
+    }
 }
