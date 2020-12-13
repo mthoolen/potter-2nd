be6f66b84c77ac14284ec113c7f457fcf9b689cc
Roy Straub <roy.straub@codesquad.nl>
2020-12-13 20:38:23 +0100

    Added VAVR

--- src/test/java/nl/codesquad/library/java/HelloTest.java
+++ src/test/java/nl/codesquad/library/java/HelloTest.java
@@ -1,11 +1,15 @@
 package nl.codesquad.library.java;
 
+import io.vavr.Tuple;
+import io.vavr.Tuple2;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HelloTest {
     @Test
     public void canary() {
-        assertTrue(false);
+        Tuple2<String, Integer> testTuple = Tuple.of("Java", 8);
+        assertEquals(9, testTuple._2);
     }
 }
