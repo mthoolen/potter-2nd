3586587ef210e0a4e57c68c5abca8e0e933762ed
Roy Straub <roy.straub@codesquad.nl>
2020-12-13 20:13:36 +0100

    No need for multi module, lets keep it simple

---
+++ src/main/kotlin/nl/codesquad/library/Hello.kt
@@ -0,0 +1,6 @@
+package nl.codesquad.library
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ src/test/kotlin/nl/codesquad/library/HelloTest.kt
@@ -0,0 +1,8 @@
+package nl.codesquad.library
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
